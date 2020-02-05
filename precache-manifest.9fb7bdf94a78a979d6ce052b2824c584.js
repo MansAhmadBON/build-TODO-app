fba27b95e843c3f73d28d9c8f4c084663e62de79
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2020-02-05 10:42:40 +0300

    init: prepared a project for deployment

---
+++ precache-manifest.9fb7bdf94a78a979d6ce052b2824c584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5004e1512a42a07e5ffcc2d97482fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2899b81b63e013cfc444",
+    "url": "/static/css/main.0c23dd24.chunk.css"
+  },
+  {
+    "revision": "57ce38078ccd4f3cd54d",
+    "url": "/static/js/2.28c96524.chunk.js"
+  },
+  {
+    "revision": "77d9a5a8d0635e146e324553e360082f",
+    "url": "/static/js/2.28c96524.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2899b81b63e013cfc444",
+    "url": "/static/js/main.a71746eb.chunk.js"
+  },
+  {
+    "revision": "b6aab384b0c1e14ae267",
+    "url": "/static/js/runtime-main.d2c0771e.js"
+  }
+]);
